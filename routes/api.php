6aee7f04ae0c1aa8f2d6394706475d912feb5af8
Angel Yahir <yahirgamerpvz@gmail.com>
2024-06-06 23:56:29 -0600

    grouped the auth routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+});
+
