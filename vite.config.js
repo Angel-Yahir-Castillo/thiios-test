776614a572f7cff35444bae6c8e23bfb0ed01552
Angel Yahir <yahirgamerpvz@gmail.com>
2024-06-08 18:11:21 -0600

    configurate vue with vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,4 +11,18 @@ export default defineConfig({
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js')
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
 });
