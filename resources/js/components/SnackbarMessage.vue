83a8689084ad61e5d7eefca11303f19e9b004ac7
Angel Yahir <yahirgamerpvz@gmail.com>
2024-06-08 18:15:07 -0600

    make the snackbarmessage component

---
+++ resources/js/components/SnackbarMessage.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-snackbar v-model="snackbarVisible" :color="color" :timeout="timeout" top>
+        {{ message }}
+        <v-btn text @click="closeSnackbar">Close</v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        props: {
+            message: {
+                type: String,
+                required: true
+            },
+            color: {
+                type: String,
+                default: 'success'
+            },
+            timeout: {
+                type: Number,
+                default: 4000
+            }
+        },
+        data() {
+            return {
+                snackbarVisible: true
+            };
+        },
+        methods: {
+            closeSnackbar() {
+                this.snackbarVisible = false;
+            }
+        }
+    };
+</script>
